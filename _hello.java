540c852b45c9262c4ba107a7e15bffc539194acc
Aashish Bedi <120207125+AashishBedi@users.noreply.github.com>
2024-01-11 22:35:12 +0530

    Add files via upload

---
+++ _hello.java
@@ -0,0 +1,6 @@
+class _hello{
+    public static void main(String[] a){
+        System.out.println("Hello World");
+    }
+}
+
---
+++ func1.java
@@ -0,0 +1,10 @@
+class func1{
+    static void show(){
+        System.out.println("This is a function inside the class");
+    }
+
+    public static void main(String args[]){
+        func1.show();
+        System.out.println("Hello World");
+    }
+}
